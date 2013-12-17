bf420ca9d00847f6e525c01e0e7ea47bcf447714
vipinkmenon <vipinkmenon@gmail.com>
2013-12-17 14:06:38 +0800

    adding v7 bitstream

--- src/hw/scripts/make_fpga.sh
+++ src/hw/scripts/make_fpga.sh
@@ -6,8 +6,8 @@ if [ $# -ne 1 ]; then
     echo "Target FPGA should be specified as ml605 or vc707"
 elif [ "$1" == "ml605" ]; then
     xtclsh ml605_cmd.tcl rebuild_project
-    cp top.bit v6_top.bit
+    cp top.bit ml605.bit
 elif [ "$1" == "vc707" ]; then
     xtclsh vc707_cmd.tcl rebuild_project
-    cp top.bit v7_top.bit
+    cp top.bit vc707.bit
 fi
